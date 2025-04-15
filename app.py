a4aecc1aaa5a59b2e3fd7c17c33db8e6c1623484
MayteGarciaCalvo <lasalastuyasyelcielodenadie@gmail.com>
2025-04-15 17:07:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ if uploaded_file is not None:
         st.subheader("📊 Resultado del análisis:")
         
         if predicted_class == 0:
-            st.error(f"🚨 Tumor detectado (probabilidad: {probability:.2f})")
-            st.markdown("### ¿Qué significa esto?\nEl modelo ha detectado la **presencia de un tumor** en la imagen. Por favor, contacta con un especialista para una evaluación profesional.")
-        elif predicted_class == 1:
-            st.success(f"✅ No se detecta tumor (probabilidad: {probability:.2f})")
-            st.markdown("### ¿Qué significa esto?\nNo se ha detectado un tumor en la imagen. Aun así, se recomienda consultar con un médico para confirmar.")
-        else:
-            st.warning(f"⚠️ Imagen no válida como resonancia (probabilidad: {probability:.2f})")
+            st.error(f"⚠️ Imagen no válida como resonancia (probabilidad: {probability:.2f})")
             st.markdown("### ¿Qué significa esto?\nLa imagen subida **no parece ser una resonancia magnética cerebral**. Asegúrate de subir una imagen válida del cerebro.")
+        elif predicted_class == 1:
+            st.success(f"🚨 Tumor detectado (probabilidad: {probability:.2f})")
+            st.markdown("### ¿Qué significa esto?\nEl modelo ha detectado la **presencia de un tumor** en la imagen. Por favor, contacta con un especialista para una evaluación profesional.")
+        else:
+            st.warning(f"✅ No se detecta tumor (probabilidad: {probability:.2f})")
+            st.markdown("### ¿Qué significa esto?\nNo se ha detectado un tumor en la imagen. Aun así, se recomienda consultar con un médico para confirmar.")
